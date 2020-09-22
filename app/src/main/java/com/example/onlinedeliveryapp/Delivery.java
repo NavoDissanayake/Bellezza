3bea6960ddc570eef65a8442c6a47a7c8fbfa56a
Piumi <aluviharepiumee@gmail.com>
2020-09-22 15:23:21 +0530

    initial commit

---
+++ app/src/main/java/com/example/onlinedeliveryapp/Delivery.java
@@ -0,0 +1,52 @@
+package com.example.onlinedeliveryapp;
+
+public class Delivery {
+
+
+    private String name;
+    private String phone;
+    private String address;
+    private String city;
+
+    public Delivery() {
+
+    }
+
+    //name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //phone number
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    //address
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+    //city
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+}
