170388b7b8113d102444dc512072e2c58e96a85a
Piumi <aluviharepiumee@gmail.com>
2020-09-22 19:49:13 +0530

    initial commit

--- app/src/main/java/com/example/onlinedeliveryapp/DeliveryActivity.java
+++ app/src/main/java/com/example/onlinedeliveryapp/DeliveryActivity.java
@@ -3,20 +3,13 @@ package com.example.onlinedeliveryapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import android.app.ProgressDialog;
 import android.text.TextUtils;
-
 import android.widget.EditText;
-
 import android.widget.Toast;
-
-
-
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -25,22 +18,18 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.HashMap;
 
-
-
-
 public class DeliveryActivity extends AppCompatActivity {
     private Button button;
-    private EditText name2,phone2,addr2,city2;
-    private String Cname , PhoneNo,Address, City,saveCurrentdate, saveCurrentTime;
-    Delivery deliver;
+    private EditText name2, phone2, addr2, city2;
+    private String saveCurrentdate, saveCurrentTime;
+    private Delivery deliver;
     private DatabaseReference DeliveryRef;
 
     private String DeliveryRandomKey, downloadImageUrl;
+    private ProgressDialog loadingBar;
 
 
-    //private ProgressDialog loadingBar;
-
-    private  void clearControls(){
+    private void clearControls() {
 
         name2.setText("");
         phone2.setText("");
@@ -49,19 +38,19 @@ public class DeliveryActivity extends AppCompatActivity {
 
     }
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delivery);
 
-        DeliveryRef = FirebaseDatabase.getInstance().getReference().child("Delivery");
 
-        button =(Button) findViewById(R.id.button2);
-        name2 = (EditText) findViewById(R.id.name1);
-        phone2 = (EditText) findViewById(R.id.phone);
-        addr2 = (EditText) findViewById(R.id.address);
-        city2 = (EditText) findViewById(R.id.city);
-        //loadingBar = new ProgressDialog(this);
+        button = findViewById(R.id.button2);
+        name2 = findViewById(R.id.name1);
+        phone2 = findViewById(R.id.phone);
+        addr2 = findViewById(R.id.address);
+        city2 = findViewById(R.id.city);
+        loadingBar = new ProgressDialog(this);
 
         deliver = new Delivery();
 
@@ -71,68 +60,61 @@ public class DeliveryActivity extends AppCompatActivity {
 
         //next button
         button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(DeliveryActivity.this, ConfirmDetailsActivity.class);
+                                      @Override
+                                      public void onClick(View view) {
+                                          Intent i = new Intent(DeliveryActivity.this, ConfirmDetailsActivity.class);
 
-                startActivity(i);
-            }
-        });
+                                          startActivity(i);
 
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                                          DeliveryRef = FirebaseDatabase.getInstance().getReference().child("Delivery");
 
 
-                try {
+                                          try {
 
-                    if (TextUtils.isEmpty(name2.getText().toString()))
-                        Toast.makeText(getApplicationContext(),"Please enter your name...", Toast.LENGTH_SHORT).show();
-                    else if (TextUtils.isEmpty(addr2.getText().toString()))
-                        Toast.makeText(getApplicationContext(),"Please enter your address...", Toast.LENGTH_SHORT).show();
-                    else if (TextUtils.isEmpty(city2.getText().toString()))
-                        Toast.makeText(getApplicationContext(),"Please enter your city...", Toast.LENGTH_SHORT).show();
-                    else{
+                                              if (TextUtils.isEmpty(name2.getText().toString()))
+                                                  Toast.makeText(getApplicationContext(), "Please enter your name...", Toast.LENGTH_SHORT).show();
+                                              else if (TextUtils.isEmpty(addr2.getText().toString()))
+                                                  Toast.makeText(getApplicationContext(), "Please enter your address...", Toast.LENGTH_SHORT).show();
+                                              else if (TextUtils.isEmpty(city2.getText().toString()))
+                                                  Toast.makeText(getApplicationContext(), "Please enter your city...", Toast.LENGTH_SHORT).show();
+                                              else {
 
-                        deliver.setName(name2.getText().toString().trim());
-                        deliver.setPhone(Integer.parseInt(phone2.getText().toString().trim()));
-                        deliver.setAddress(addr2.getText().toString().trim());
-                        deliver.setCity(city2.getText().toString().trim());
-
-
-                        DeliveryRef.push().setValue(deliver);
-
-                        Toast.makeText(getApplicationContext(),"successfully inserted...", Toast.LENGTH_SHORT).show();
-                        clearControls();
+                                                  deliver.setName(name2.getText().toString().trim());
+                                                  deliver.setPhone(Integer.parseInt(phone2.getText().toString().trim()));
+                                                  deliver.setAddress(addr2.getText().toString().trim());
+                                                  deliver.setCity(city2.getText().toString().trim());
 
 
+                                                  DeliveryRef.push().setValue(deliver);
 
-                    }
-
-                }
-                catch(NumberFormatException e){
-
-                    Toast.makeText(getApplicationContext(),"invalid contact number....", Toast.LENGTH_SHORT).show();
-
-                }
-
-            }
-        });
-
-       // StoreProductInformation();
+                                                  Toast.makeText(getApplicationContext(), "successfully inserted...", Toast.LENGTH_SHORT).show();
+                                                  clearControls();
 
 
+                                              }
+
+                                          } catch (NumberFormatException e) {
+
+                                              Toast.makeText(getApplicationContext(), "invalid contact number....", Toast.LENGTH_SHORT).show();
+
+                                          }
+
+                                          StoreProductInformation();
+
+                                      }
+
+
+                                  }
+
+        );
+
+
+        
     }
 
 
-
-
-
-
-   /* private void StoreProductInformation() {
-
-
+    private void StoreProductInformation () {
 
 
         //loadingBar.setIcon(R.drawable.plus);
@@ -142,53 +124,17 @@ public class DeliveryActivity extends AppCompatActivity {
         loadingBar.show();
 
 
+        Calendar calendar = Calendar.getInstance();
 
-            Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat currentDate = new SimpleDateFormat("MMM dd, yyyy");
+        saveCurrentdate = currentDate.format(calendar.getTime());
 
-            SimpleDateFormat currentDate = new SimpleDateFormat("MMM dd, yyyy");
-            saveCurrentdate = currentDate.format(calendar.getTime());
+        SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss a");
+        saveCurrentTime = currentTime.format(calendar.getTime());
 
-            SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss a");
-            saveCurrentTime = currentTime.format(calendar.getTime());
-
-            DeliveryRandomKey = saveCurrentdate + saveCurrentTime;
-
+        DeliveryRandomKey = saveCurrentdate + saveCurrentTime;
 
 
-        }
-*/
-
-    /*    private void SaveProductInfoToDatabase ()
-        {
-            HashMap<String, Object> productMap = new HashMap<>();
-
-            productMap.put("DeliveryId", DeliveryRandomKey);
-            productMap.put("date", saveCurrentdate);
-            productMap.put("time", saveCurrentTime);
-            productMap.put("CustomerName", Cname);
-            productMap.put("PhoneNo", PhoneNo);
-            productMap.put("Address", Address);
-            productMap.put("City", City);
-
-            DeliveryRef.child(DeliveryRandomKey).updateChildren(productMap)
-                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            if (task.isSuccessful()) {
-                                Intent intent1 = new Intent(DeliveryActivity.this, ConfirmDetailsActivity.class);
-                                startActivity(intent1);
-
-                                loadingBar.dismiss();
-                                Toast.makeText(DeliveryActivity.this, " Successfull..", Toast.LENGTH_SHORT).show();
-                            } else {
-                                loadingBar.dismiss();
-                                String message = task.getException().toString();
-                                Toast.makeText(DeliveryActivity.this, "Error: " + message, Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
-        }
-
-
-     */
     }
+
+}
