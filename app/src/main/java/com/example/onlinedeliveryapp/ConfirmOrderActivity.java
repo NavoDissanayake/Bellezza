c66113b22bd8529e964b1aade0ee010953a96357
Piumi <aluviharepiumee@gmail.com>
2020-09-24 13:03:17 +0530

    initial commit

--- app/src/main/java/com/example/onlinedeliveryapp/ConfirmOrderActivity.java
+++ app/src/main/java/com/example/onlinedeliveryapp/ConfirmOrderActivity.java
@@ -1,20 +1,62 @@
 package com.example.onlinedeliveryapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ConfirmOrderActivity extends AppCompatActivity {
     Button button;
+    TextView confirmName,confirmPhone,confirmAddress,confirmCity;
+    Delivery deliver;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm_order);
+
+
+        confirmName = findViewById(R.id.name2);
+        confirmPhone=findViewById(R.id.phone2);
+        confirmAddress= findViewById(R.id.address2);
+        confirmCity=findViewById(R.id.city2);
+
+
+        deliver = new Delivery();
+
+        DatabaseReference readRef = FirebaseDatabase.getInstance().getReference().child("Delivery").child("delivery1");
+
+
+        readRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.hasChildren()) {
+                    confirmName.setText(dataSnapshot.child("name").getValue().toString());
+                    confirmPhone.setText(dataSnapshot.child("phone").getValue().toString());
+                    confirmAddress.setText(dataSnapshot.child("address").getValue().toString());
+                    confirmCity.setText(dataSnapshot.child("city").getValue().toString());
+
+                } else
+                    Toast.makeText(getApplicationContext(), "No source to Display", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
 
         //action bar
@@ -29,6 +71,11 @@ public class ConfirmOrderActivity extends AppCompatActivity {
             public void onClick(View view) {
                 openDialog();
             }
+
+
+
+
+
         });
 
 
@@ -40,6 +87,10 @@ public class ConfirmOrderActivity extends AppCompatActivity {
         confirmdialog.show(getSupportFragmentManager(),"confirm dialog");
 
     }
+
+
+
+
 
 
 }
