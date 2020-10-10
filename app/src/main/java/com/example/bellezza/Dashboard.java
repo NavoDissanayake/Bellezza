a9ea488978eca42f57ada8db3e171fa990f30561
IT19207346 <63913339+IT19207346@users.noreply.github.com>
2020-10-10 22:21:13 +0530

    initial commit

--- app/src/main/java/com/example/bellezza/Dashboard.java
+++ app/src/main/java/com/example/bellezza/Dashboard.java
@@ -1,14 +1,57 @@
 package com.example.bellezza;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class Dashboard extends AppCompatActivity {
+
+    ListView listView;
+    FirebaseDatabase db;
+    DatabaseReference dbref;
+    ArrayList<Face>list;
+    ArrayAdapter<Face> adapter;
+    Face face;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+
+        listView = (ListView)findViewById(R.id.list);
+        db= FirebaseDatabase.getInstance();
+        dbref=db.getReference("faces");
+        list=new ArrayList<>();
+        adapter=new ArrayAdapter<Face>(this,R.layout.productinfo,R.id.face_name,list);
+
+        dbref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot da: dataSnapshot.getChildren())
+                {
+                    face=da.getValue(Face.class);
+                    list.add(face);
+                }
+                listView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
