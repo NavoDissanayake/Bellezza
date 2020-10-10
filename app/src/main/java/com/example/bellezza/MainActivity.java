9b9803e3cf9820891c661ae79a8462f36a72a100
IT19207346 <63913339+IT19207346@users.noreply.github.com>
2020-10-10 21:37:09 +0530

    initial commit

--- app/src/main/java/com/example/bellezza/MainActivity.java
+++ app/src/main/java/com/example/bellezza/MainActivity.java
@@ -2,13 +2,52 @@ package com.example.bellezza;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN=5000;
+
+    //variables
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView logo,slogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main2);
+
+        //Animation
+        topAnim= AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim=AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        //hooks
+        image=findViewById(R.id.imageView5);
+        logo=findViewById(R.id.bellezza);
+        slogan=findViewById(R.id.text);
+
+
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Dashboard.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
     }
 }
