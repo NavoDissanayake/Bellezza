29d952914d128e4797fa3cf0dde84f8c28f73e3b
IT19207346 <63913339+IT19207346@users.noreply.github.com>
2020-10-12 20:50:17 +0530

    initial commit

--- app/src/main/java/com/example/bellezza/AdminReg.java
+++ app/src/main/java/com/example/bellezza/AdminReg.java
@@ -2,22 +2,25 @@ package com.example.bellezza;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class AdminReg extends AppCompatActivity {
 
-    EditText Adname,Adpass,Ademail,Adphone,Adnic;
-    Button sinup,loging;
-
+    EditText Adname, Adpass, Ademail, Adphone, Adnic, Adconpass;
+    Button sinup, loging;
+    AlertDialog loadingBar;
 
     DatabaseReference databaseReference;
 
@@ -26,11 +29,11 @@ public class AdminReg extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_reg);
 //add activity
-        Button button = (Button)findViewById(R.id.loging);
+        Button button = (Button) findViewById(R.id.loging);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent9 = new Intent(AdminReg.this ,AdminLogin.class);
+                Intent intent9 = new Intent(AdminReg.this, AdminLogin.class);
                 startActivity(intent9);
 
                 //Add Toast
@@ -39,17 +42,17 @@ public class AdminReg extends AppCompatActivity {
         });
 
 
-
-
         databaseReference = FirebaseDatabase.getInstance().getReference("Admin");
 
+        //Assign Variables
 
         Adname = (EditText) findViewById(R.id.name);
         Ademail = (EditText) findViewById(R.id.Email);
-        Adphone=(EditText) findViewById(R.id.phone);
-        Adnic=(EditText) findViewById(R.id.Email);
-        Adpass=(EditText) findViewById(R.id.pass);
-       sinup = (Button)findViewById(R.id.sinup);
+        Adphone = (EditText) findViewById(R.id.phone);
+        Adnic = (EditText) findViewById(R.id.nic);
+        Adpass = (EditText) findViewById(R.id.pass);
+        Adconpass = (EditText) findViewById(R.id.confpass);
+        sinup = (Button) findViewById(R.id.sinup);
 
         sinup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,41 +61,87 @@ public class AdminReg extends AppCompatActivity {
             }
         });
 
-
     }
 
-    private void Administrator(){
+
+    boolean isEmail(EditText text) {
+        CharSequence email = text.getText().toString();
+        return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+    }
+
+    boolean isEmpty(EditText text) {
+        CharSequence str = text.getText().toString();
+        return TextUtils.isEmpty(str);
+    }
+
+
+    private void Administrator() {
 
         //getting the values to save
 
         String name = Adname.getText().toString().trim();
-        String email= Ademail.getText().toString().trim();
-        String nic= Adnic.getText().toString().trim();
-        String phone =Adphone.getText().toString().trim();
-        String pwd =Adpass.getText().toString().trim();
+        String email = Ademail.getText().toString().trim();
+        String nic = Adnic.getText().toString().trim();
+        String phone = Adphone.getText().toString().trim();
+        String pwd = Adpass.getText().toString().trim();
+        String cpwd = Adconpass.getText().toString().trim();
+
 
 //checking
-        if (!TextUtils.isEmpty(name)){
+        if (!TextUtils.isEmpty(name)) {
 
             //getting the unique id using push().getKey() method
             //it create Id As a primary key
-            String id= databaseReference.push().getKey();
+            String id = databaseReference.push().getKey();
 
             //creating
-            Administrator administrator = new Administrator(id, name ,email,phone,pwd);
+            Administrator administrator = new Administrator(id, name, email, phone, pwd,nic,cpwd);
 
             //saving
             databaseReference.child(id).setValue(administrator);
 
             //displaying Success toast
-            Toast.makeText(this,"Account Created Successfully",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Account Created Successfully", Toast.LENGTH_SHORT).show();
 
 
-        }else{
-            //if the value is not given displaying toast
-            Toast.makeText(this,"You Should Enter Name",Toast.LENGTH_SHORT).show();;
         }
 
+        if (isEmail(Ademail) == false) {
+            Ademail.setError("Enter valid email!");
+        }
+        if(isEmpty(Adpass))
+        {
+            Adpass.setError("Enter your password.");
+        }
 
+        if(isEmpty(Adconpass))
+        {
+           Adconpass.setError("Enter your confirmation password");
+
+            if (!Adconpass.equals(Adpass))
+            {
+                Toast.makeText(AdminReg.this, "Password do not match", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        else {
+
+            loadingBar.setTitle("Creating New Account");
+            loadingBar.setMessage("Please wait while we are creating account for you.");
+            loadingBar.show();
+            Task task = null;
+            if(task.isSuccessful())
+            {
+                Toast.makeText(AdminReg.this, "You have successfully signed up", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+            else
+                {
+                    Toast.makeText(AdminReg.this, "Error occured, please try again.", Toast.LENGTH_SHORT).show();
+                }
+                loadingBar.dismiss();
+
+        }
     }
 }
+
--- app/src/main/java/com/example/bellezza/Administrator.java
+++ app/src/main/java/com/example/bellezza/Administrator.java
@@ -7,15 +7,22 @@ public class Administrator {
     String AdminEmail;
     String phone;
     String password;
+    String ConfirmPassword;
     String NIC;
 
-    public Administrator(String adminId, String adminName, String adminEmail, String phone, String password) {
+
+
+    public Administrator(String adminId, String adminName, String adminEmail, String phone, String password, String confirmPassword) {
         AdminId = adminId;
         AdminName = adminName;
         AdminEmail = adminEmail;
         this.phone = phone;
         this.password = password;
         this.NIC = NIC;
+        this.ConfirmPassword = confirmPassword;
+    }
+
+    public Administrator(String id, String name, String email, String phone, String pwd, String nic, String cpwd) {
     }
 
     public String getAdminId() {
@@ -46,5 +53,7 @@ public class Administrator {
     public String getPassword() {
         return password;
     }
-
+    public String getConfirmPassword() {
+        return ConfirmPassword;
+    }
 }
